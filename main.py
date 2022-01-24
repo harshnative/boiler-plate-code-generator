0c4999be11057418b6a3a114b19f5f08598b99ce
harshnative <harshnative@gmail.com>
2022-01-24 15:55:47 +0530

    added PathFunctions class to main.py
    added getSubFolders and getSubFiles method to class PathFunctions to main.py
    
    Signed-off-by: harshnative <harshnative@gmail.com>

---
+++ main.py
@@ -0,0 +1,86 @@
+import pathlib
+import os
+import sys
+
+
+
+
+
+# function to get the path from the relative path of file
+# required when file is bundled with pyinstaller
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+
+
+
+
+
+
+
+# class containing path lib methods
+class PathFunctions:
+
+
+    # method to return all sub folders inside a dir
+    @classmethod
+    def getSubFolders(cls , root_path):
+
+        pathList = []
+
+        dirpath = pathlib.Path(root_path)
+
+        assert dirpath.is_dir()
+
+        for path in dirpath.iterdir():
+            if path.is_dir():
+                pathList.append(path)
+
+        return pathList
+
+
+
+    # method to return all sub files inside a dir
+    # does not include files inside sub dirs
+    @classmethod
+    def getSubFiles(cls , folder_path):
+
+        filesList = []
+
+        dirpath = pathlib.Path(folder_path)
+
+        assert dirpath.is_dir()
+
+        for x in dirpath.iterdir():
+            if x.is_file():
+                filesList.append(x)
+
+        return filesList
+
+
+
+
+
+
+
+
+def main():
+    print(PathFunctions.getSubFolders(resource_path("templates/")))
+    input()
+    print(PathFunctions.getSubFiles(resource_path("templates/py/")))
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
